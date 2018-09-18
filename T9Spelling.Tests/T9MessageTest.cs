f26669492f3fbda79f116a63cb4d7a6523b2311b
Dmitriy Garbuz <dmigar@gmail.com>
2018-09-18 17:43:10 +0300

    SingleCharacterGivesRightSequence +

--- T9Spelling.Tests/T9MessageTest.cs
+++ T9Spelling.Tests/T9MessageTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace T9Spelling
@@ -11,6 +13,46 @@ namespace T9Spelling
             {
                 Latin = string.Empty
             }.Digital);
+        }
+
+        [Fact]
+        public void SingleCharacterGivesRightSequence()
+        {
+            new Dictionary<char, string>()
+            {
+                { ' ', "1"   },
+                { 'a', "2"   },
+                { 'b', "22"  },
+                { 'c', "222" },
+                { 'd', "3"   },
+                { 'e', "33"  },
+                { 'f', "333" },
+                { 'g', "4"   },
+                { 'h', "44"  },
+                { 'i', "444" },
+                { 'j', "5"   },
+                { 'k', "55"  },
+                { 'l', "555" },
+                { 'm', "6"   },
+                { 'n', "66"  },
+                { 'o', "666" },
+                { 'p', "7"   },
+                { 'q', "77"  },
+                { 'r', "777" },
+                { 's', "7777"},
+                { 't', "8"   },
+                { 'u', "88"  },
+                { 'v', "888" },
+                { 'w', "9"   },
+                { 'x', "99"  },
+                { 'y', "999" },
+                { 'z', "9999"},
+                { '+', "0"   },
+            }
+            .ToList()
+            .ForEach(x => Assert.Equal(
+                x.Value, 
+                new T9Message() { Latin = x.Key.ToString() }.Digital));
         }
     }
 }
